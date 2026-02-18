6a733407898e6a23b15a602fe40087804f134acd
manueldezman <adelekeabdulganiy@gmail.com>
2026-02-18 14:49:00 +0100

    refactor: update the code to use datasets

--- index.js
+++ index.js
@@ -1,102 +1,122 @@
+
 const myLibrary = [];
+
 
 class Book {
     constructor (title, author, pages, read) {
-  
         this.title = title;
         this.author = author;
         this.pages = pages;
         this.read = read;
+        this.id = crypto.randomUUID();
     }    
 
     info() {
         return `${this.title} by ${this.author}, has ${this.pages} pages.`
     }
-
-} 
-
-
-function addBookToLibrary() {
-    if (title.value != "" && author.value != "" && pages.value != "") {
-        let ReadData;
-    
-        if (checkBox.checked) {
-            ReadData = "Yes";
-        }
-        else {
-            ReadData = "No";
-        }
-        const bookName = new Book(title.value, author.value, pages.value, ReadData);
-        myLibrary.push(bookName);
-        console.log(myLibrary);
-        displayBook();
-        title.value = "";
-        author.value = "";
-        pages.value = "";
-        checkBox.checked = false;
-
-    }
-    }
-function displayBook() {
-    table.textContent = "";
-
-    for (book of myLibrary) {
-        const row = document.createElement("tr");
-        row.setAttribute("id", book.title);
-        const titleData = document.createElement("td");
-        titleData.textContent = book.title;
-        row.appendChild(titleData);
-        
-        const authorData = document.createElement("td");
-        authorData.textContent = book.author;
-        row.appendChild(authorData);
-
-        const pagesData = document.createElement("td");
-        pagesData.textContent = book.pages;
-        pagesData.classList.add("pages");
-        row.appendChild(pagesData);
-
-        
-        const ReadBtn = document.createElement("td");
-        let index = myLibrary.indexOf(book);
-
-        ReadBtn.innerHTML = `<button id="${index}" onclick="toggleRead(${index})" class="ReadBtn"> ${book.read}</button>`;
-        row.appendChild(ReadBtn);
-
-
-        const ActionsData = document.createElement("td");
-        const DeleteBtn = document.createElement("button");
-        DeleteBtn.textContent = "Delete";
-        DeleteBtn.setAttribute("id", book.title);
-        ActionsData.appendChild(DeleteBtn);
-        
-        row.appendChild(ActionsData);
-
-        table.appendChild(row);
-
-    }
-}   
-
-function toggleRead(id) {
-    if (myLibrary[id].read === "Yes") {
-        myLibrary[id].read = "No";
-    } 
-    else if (myLibrary[id].read === "No") {
-        myLibrary[id].read = "Yes";
-    }
-
-    displayBook();
 }
-
-const AddNewBookBtn = document.querySelector(".addNewBook");
-const AddBookBtn = document.querySelector(".addBook");
 
 
 const title = document.querySelector("#title");
 const author = document.querySelector("#author");
 const pages = document.querySelector("#pages");
-const table = document.querySelector(".tbody");
 const checkBox = document.querySelector("#read");
+const table = document.querySelector(".tbody");
+const tableRow = document.querySelector("tbody");
+
+const AddBookBtn = document.querySelector(".addBook");
+
+
+
+function createBook() {
+
+    let name = title.value;
+    let authorVal = author.value;
+    let pagesVal = pages.value;
+    let readStatus = (checkBox.checked) ? "Yes" : "No";
+
+    let book = new Book(name, authorVal, pagesVal, readStatus);
+
+    storeBook(book);
+
+    displayBooks();
+}
+
+function storeBook(book) {
+    myLibrary.push(book);
+}
+
+function deleteBooks(event) {
+
+    let toBeRemoved = event.target;
+
+    for (let item of myLibrary) {
+        if (toBeRemoved.dataset.id === item.id) {
+
+            let index = myLibrary.indexOf(item);
+            myLibrary.splice(index, 1);
+
+            displayBooks();
+        }
+    }
+}
+
+function displayBooks() {
+
+    tableRow.textContent = "";
+
+    for (let item of myLibrary) {
+
+        let bookRow = document.createElement("tr");
+        let bookName = document.createElement("td");
+        let bookAuthor = document.createElement("td");
+        let pagesNumber = document.createElement("td");
+        let readStatusCell = document.createElement("td");
+        let action = document.createElement("td");
+        let deleteBtn = document.createElement("button");
+        let readBtn = document.createElement("button");
+
+        bookRow.dataset.id = item.id;
+        deleteBtn.dataset.id = item.id;
+        readBtn.dataset.id = item.id;
+
+        bookName.textContent = item.title;
+        bookAuthor.textContent = item.author;
+        pagesNumber.textContent = item.pages;
+        readBtn.textContent = item.read;
+        deleteBtn.textContent = "Delete";
+
+        readStatusCell.appendChild(readBtn);
+        action.appendChild(deleteBtn);
+        bookRow.appendChild(bookName);
+        bookRow.appendChild(bookAuthor);
+        bookRow.appendChild(pagesNumber);
+        bookRow.appendChild(readStatusCell);
+        bookRow.appendChild(action);
+
+        tableRow.appendChild(bookRow);
+
+        deleteBtn.addEventListener("click", deleteBooks);
+        readBtn.addEventListener("click", changeReadStatusOnCLick);
+    }
+}
+
+function changeReadStatusOnCLick(event) {
+
+    for (let item of myLibrary) {
+        if (event.target.dataset.id === item.id) {
+            item.toggleReadStatus();
+            displayBooks();
+        }
+    }
+}
+
+
+
+Book.prototype.toggleReadStatus = function() {
+   this.read = (this.read === "Yes") ? "No" : "Yes";
+}
+
 
 
 const Book1 = new Book("Think and grow Rich", "Napoleon Hill", "250", "Yes");
@@ -104,26 +124,9 @@ const Book2 = new Book("Atomic Habits", "James Clear", "260", "Yes");
 const Book3 = new Book("Richest Man in Babylon", "George Carson", "144", "Yes");
 const Book4 = new Book("Permission Marketing", "Seth Godin", "224", "Yes");
 
-myLibrary.push(Book1);
-myLibrary.push(Book2);
-myLibrary.push(Book3);
-myLibrary.push(Book4);
+myLibrary.push(Book1, Book2, Book3, Book4);
 
-displayBook();
+displayBooks();
 
 
-
-AddBookBtn.addEventListener("click", addBookToLibrary);
-
-table.addEventListener("click", function(event) {
-    
-    for (book of myLibrary) {
-        if (event.target.id === book.title) {
-            let index = myLibrary.indexOf(book);
-
-            myLibrary.splice(index, 1);
-
-            displayBook();
-        }
-    }
-});
+AddBookBtn.addEventListener("click", createBook);

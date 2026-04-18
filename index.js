2804bb05b1a32e3db3c036462b3d6fa72082e42e
manueldezman <adelekeabdulganiy@gmail.com>
2026-04-18 03:03:02 +0100

    feat: add javaScipt form validation

--- index.js
+++ index.js
@@ -23,9 +23,66 @@ const pages = document.querySelector("#pages");
 const checkBox = document.querySelector("#read");
 const table = document.querySelector(".tbody");
 const tableRow = document.querySelector("tbody");
-
+const form = document.querySelector(".modal-container");
 const AddBookBtn = document.querySelector(".addBook");
 
+const errorMessages = {
+    titleErrorMsg: document.querySelector(".name"),
+    authorErrorMsg: document.querySelector(".author"),
+    pagesErrorMsg: document.querySelector(".pages"),
+}
+
+
+
+
+
+function handleValidation(event) {
+
+    const errorName = event.target.id;
+    const inputVar = event.target;
+    const key = `${errorName}ErrorMsg`;
+
+    const targetDiv = errorMessages[key];
+
+    if (inputVar.validity.valid) {
+        targetDiv.textContent = "";
+        targetDiv.classList.remove("error");
+    }
+    else {
+         targetDiv.classList.add("error");
+        showError(errorName, inputVar);
+    }
+}
+
+
+
+function showError(errorName, inputVar) {
+
+    const key = `${errorName}ErrorMsg`;
+    const targetDiv = errorMessages[key];
+    if (inputVar.validity.valueMissing) {
+
+        targetDiv.textContent = `The ${errorName} field is required and cannot be left blank.`;
+        targetDiv.classList.remove("hidden");
+    }
+     else if (inputVar.validity.rangeOverflow) {
+        targetDiv.textContent = `The value must be below 1000 pages`;
+        targetDiv.classList.remove("hidden");
+    }
+    else if (inputVar.validity.rangeUnderflow) {
+        targetDiv.textContent = "The value must be above 20 pages";
+        targetDiv.classList.remove("hidden");
+    }
+    else if (inputVar.validity.patternMismatch) {
+        targetDiv.textContent = `The format entered is invalid, this field only accept text.`;
+        targetDiv.classList.remove("hidden");
+    }
+    else if (inputVar.validity.tooShort) {
+        targetDiv.textContent = "This input is too short, it must be at least 5 characters.";
+        targetDiv.classList.remove("hidden");
+    }
+   
+}
 
 
 function createBook(event) {
@@ -135,8 +192,25 @@ const Book4 = new Book("Permission Marketing", "Seth Godin", "224", "Yes");
 
 myLibrary.push(Book1, Book2, Book3, Book4);
 
+form.addEventListener("submit", () => {
+
+    if (!title.validity.valid || !author.validity.valid || !pages.validity.valid) {
+
+        event.preventDefault();
+
+        return;
+    }
+    else {
+        createBook(event);
+    }
+
+});
+
+title.addEventListener("input", handleValidation)
+author.addEventListener("input", handleValidation)
+pages.addEventListener("input", handleValidation);
+
 displayBooks();
 
 
-const form = document.querySelector(".modal-container");
-form.addEventListener("submit", createBook);
+
